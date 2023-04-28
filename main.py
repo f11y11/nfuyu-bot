529e1ef13c8ca2b56baff53dba931369bd41520e
f11y11 <me@fuyu.gg>
2023-04-29 00:38:32 +0300

    fix: inconsistency with logging

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import asyncio
 from dotenv import load_dotenv
 from bot.bot import main, config
 
+logging_data = config.get('logging')
+load_dotenv()
 
-if logging_data := config.get('logging'):
+if __name__ == '__main__':
     if logging_level := logging_data.get('level'):
         if logging_level and logging_level not in (
             'error',
@@ -16,25 +18,20 @@ if logging_data := config.get('logging'):
         ):
             logging_level = 'info'
 
-            if logging_filename := logging_data.get('filename'):
-                if not logging_filename.endswith('.log'):
-                    logging_filename = logging_filename + '.log'
+    if logging_filename := logging_data.get('filename'):
+        if not logging_filename.endswith('.log'):
+            logging_filename = logging_filename + '.log'
 
-            logging_console: bool = logging_data.get('console', False)
+    if logging_data.get('console', False):
+        logging.getLogger().addHandler(logging.StreamHandler())
+    elif not logging_filename:
+        print('''
+            You have enabled logging but set left both console and filename blank.
+            Nothing will be printed to the console or written to a file. 
+            ''')
 
-            logging.basicConfig(level=getattr(logging, logging_level.upper()), filename=logging_filename)
-            if logging_console:
-                logging.getLogger().addHandler(logging.StreamHandler())
+    logging.basicConfig(level=getattr(logging, logging_level.upper()), filename=logging_filename)
 
-            if not logging_console and not logging_filename:
-                print('''
-                You have enabled logging but set left both console and filename blank.
-                Nothing will be printed to the console or written to a file. 
-                ''')
-
-load_dotenv()
-
-if __name__ == '__main__':
     if TOKEN := os.environ.get('TOKEN'):
         logging.info('Starting bot')
         asyncio.run(main(TOKEN))
