ec4c07214aa4615ca6febf670030a062263a6029
f11y11 <ymntbt@gmail.com>
2023-09-09 13:00:47 +0300

    feat: enhance logging

--- main.py
+++ main.py
@@ -5,40 +5,34 @@ import asyncio
 from dotenv import load_dotenv
 from bot.bot import main, config
 
-logging_data = config.get("logging")
-load_dotenv()
+logging_data = config.get("logging", {})
+if not isinstance(logging_data, dict):
+    logging_data = dict()
+
+
+if not os.environ.get("TOKEN"):
+    load_dotenv()
+
+if level := logging_data.get("level"):
+    handlers = []
+    formatter = logging.Formatter("%(asctime)s %(name)-16s %(levelname)-8s %(message)s")
+    if filename := logging_data.get("filename"):
+        if not filename.endswith(".log"):
+            filename += ".log"
+
+        file_handler = logging.FileHandler(filename)
+        file_handler.setFormatter(formatter)
+        handlers.append(file_handler)
+    if logging_data.get("console"):
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        handlers.append(stream_handler)
+    logging.basicConfig(level=getattr(logging, level.upper()), handlers=handlers)
 
 if __name__ == "__main__":
-    if logging_level := logging_data.get("level"):
-        if logging_level and logging_level not in (
-            "error",
-            "info",
-            "debug",
-            "critical",
-        ):
-            logging_level = "info"
-
-    if logging_filename := logging_data.get("filename"):
-        if not logging_filename.endswith(".log"):
-            logging_filename = logging_filename + ".log"
-
-    if logging_data.get("console", False):
-        logging.getLogger().addHandler(logging.StreamHandler())
-    elif not logging_filename:
-        print(
-            """
-            You have enabled logging but set both console and filename blank.
-            Nothing will be printed to the console or written to a file. 
-            """
-        )
-
-    logging.basicConfig(
-        level=getattr(logging, logging_level.upper()), filename=logging_filename
-    )
-
     if TOKEN := os.environ.get("TOKEN"):
         logging.info("Starting bot")
         asyncio.run(main(TOKEN))
     else:
-        logging.error("No TOKEN in .!env")
+        logging.error("No TOKEN in .env")
         logging.error("Generate an example .env file using cp .env-template .env")
